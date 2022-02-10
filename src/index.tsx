131ffef7a2a85af20a489b8a0aab6e9439cfdf17
busyzz <540548050@qq.com>
2022-02-10 09:54:57 +0800

    feat: support sentry

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import 'antd/dist/antd.css';
 import './styles/index.scss';
+// 如需使用 sentry 监控异常 , 设置自己的 REACT_APP_SENTRY_DSN 环境变量
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
 // React.StrictMode
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,6 +6,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
+    readonly REACT_APP_SENTRY_DSN: string;
   }
 }
 declare module '*.avif' {
